39848c8adfbb9b1891afae13a99c7e7105c790c9
mcit39 <mattioc@protonmail.com>
2026-09-25 17:41:20 -0600

    sintetizar.sh: el grep del final tumbaba uart.sh cuando yosys no imprimia nada

--- docs/diseño/diagramas/esquematicos/sintetizar.sh
+++ docs/diseño/diagramas/esquematicos/sintetizar.sh
@@ -5,4 +5,4 @@ yosys -q -p "read_verilog -sv $sv $EXTRA; ${cp:+chparam $cp $top;} hierarchy -to
 python3 "$D/agrupar.py" pre.json $top $sv "$@"
 for g in $conos; do python3 "$D/conos.py" pre.json $top $g; done
 # keep evita que abc se trague las señales con nombre
-yosys -q -p "read_json pre.json; ${EXTRA:+read_verilog -sv $EXTRA;} hierarchy -top $top; submod; setattr -set keep 1 w:[a-zA-Z]*; opt; wreduce; opt; techmap; opt -fast; dfflegalize -cell \$_DFF_P_ x; abc -g AND,OR,XOR,MUX; opt_clean; write_json $top.all.json" 2>&1 | grep -v combinational
+yosys -q -p "read_json pre.json; ${EXTRA:+read_verilog -sv $EXTRA;} hierarchy -top $top; submod; setattr -set keep 1 w:[a-zA-Z]*; opt; wreduce; opt; techmap; opt -fast; dfflegalize -cell \$_DFF_P_ x; abc -g AND,OR,XOR,MUX; opt_clean; write_json $top.all.json" 2>&1 | { grep -v combinational || true; }
